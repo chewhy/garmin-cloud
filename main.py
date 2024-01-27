1c86e4b2a43d33361f9f27f82dd8a1e3645d9be1
Hong Ye Chew <hongye.chew@gmail.com>
2024-01-27 18:12:22 +0800

    GC-5 main.py now uploads data.json to GCS.

--- main.py
+++ main.py
@@ -2,14 +2,19 @@
 import datetime
 import json
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from garminconnect import Garmin
-from test_write_gcs import create_client_from_json
+from test_write_gcs import (create_client_from_json,
+                            upload_file)
+
+
+#TODO use file-like object io.StringIO to upload to GCS without creating a file locally
 
 username = os.environ["ID"]
 password = os.environ["PASS"]
 
 def init_api():
+    """Initialize API for Garmin Connect."""
     _api = Garmin(username, password)
     _api.login()
 
@@ -20,6 +25,7 @@ if __name__ == "__main__":
 
     SERVICE_KEY_PATH = r'atd2024-4c3b61c5ad99.json'
     BUCKET_NAME = "test-storage-abc"
+    DATA_FILE_PATH = r'data.json'
 
     start_date = datetime.date(2019, 1, 1)
     end_date = datetime.date(2023, 12, 30)
@@ -29,8 +35,10 @@ if __name__ == "__main__":
     activities = api.get_activities_by_date(
                     start_date.isoformat(), end_date.isoformat())
 
-    with open("data.json", "w", encoding="utf-8") as file:
+    with open(DATA_FILE_PATH, "w", encoding="utf-8") as file:
         json.dump(activities, file, indent=4)
 
     gcs_client = create_client_from_json(SERVICE_KEY_PATH)
     gcs_bucket = gcs_client.bucket(BUCKET_NAME)
+
+    upload_file(gcs_bucket, DATA_FILE_PATH)
